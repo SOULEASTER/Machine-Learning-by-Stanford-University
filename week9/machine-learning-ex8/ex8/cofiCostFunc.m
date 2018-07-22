00fbdd24b39e999fa577dc2b787fc87c8f0f8992
Rajer <souleater.rajer@gmail.com>
2018-07-23 01:01:19 +0800

    week9-ex finish

--- week9/machine-learning-ex8/ex8/cofiCostFunc.m
+++ week9/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -39,21 +39,10 @@ Theta_grad = zeros(size(Theta));
 %        Theta_grad - num_users x num_features matrix, containing the 
 %                     partial derivatives w.r.t. to each element of Theta
 %
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+J = 1 / 2 * sum(sum(((X * Theta') .^ R - Y .^ R) .^ 2)) + lambda / 2 * sum(sum(Theta .^ 2)) + lambda / 2 * sum(sum(X .^ 2));
+ 
+X_grad = ((X * Theta') .* R - Y .* R) * Theta + lambda .* X;
+Theta_grad = ((X * Theta') .* R - Y .* R)' * X + lambda .* Theta;
 
 % =============================================================
 
